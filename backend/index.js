2b11afb2fab37573e1264c0807c66cf773cbfaa4
Leonides Hidalgo <leonideha@gmail.com>
2025-07-25 22:58:29 -0400

    agregando proyecto

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("server online");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("server corriendo");
+});
