71fe05c885c55f98b780bf074da64180ea1bfeb5
Leonides Hidalgo <leonideha@gmail.com>
2025-07-28 16:35:52 -0400

    Actualizando

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AlzaSyAEfNB6U96_i4kmBTU4crAKXya1ZQXDpks",
+  authDomain: "peluqueria-web-792f0.firebase.com",
+  projectId: "peluqueria-web-792f0",
+  storageBucket: "peluqueria-web-792f0.firebaseapp.com",
+  messagingSenderId: "687230116423",
+  appId: "1:687230116423:web:0cbe5688796a4a7b270762",
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta los servicios que vas a usar
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,58 @@
 import "./Home.css";
+import { auth, db } from "../firebase";
+import { useEffect, useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+
+interface Appointment {
+  id: string;
+  [key: string]: any; // This allows for other properties from doc.data()
+}
+
 export default function Home() {
+  function handleClickAuth() {
+    // Logic for authentication button click
+    console.log("Authentication button clicked");
+    console.log("User:", user);
+    console.log("Appointments:", appointments);
+  }
+  const [user] = useState({
+    displayname: "Hansel Rodriguez",
+    email: "hansel.lunrak@gmail.com",
+    uid: "abc123xyz",
+  });
+  //type Appointment = { id: string } & Record<string, any>;
+  //const [appointments, setAppointments] = useState<Appointment | null>(null);
+  const [appointments, setAppointments] = useState<Appointment | null>(null);
+  useEffect(() => {
+    const loadAppointment = async () => {
+      try {
+        const q = query(
+          collection(db, "Appointments"),
+          where("userId", "==", "abc123xyz")
+        );
+        console.log("EJECUTANDO QUERY");
+        console.log("Query:", q);
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const doc = querySnapshot.docs[0];
+          console.log(doc);
+          setAppointments({ id: doc.id, ...doc.data() });
+        }
+      } catch (error) {
+        console.error("Error al cargar la cita:", error);
+      }
+    };
+    loadAppointment();
+  }, []);
+
   return (
     <>
       <header className="header_home">
         <img className="imglogo_home" src="/public/icon.png" />
         <h2>Peluquería Tal!</h2>
-        <button className="button_auth_home">Autentificarse</button>
+        <button onClick={handleClickAuth} className="button_auth_home">
+          Autentificarse
+        </button>
       </header>
       <section className="main_home">
         <img src="/public/portada.jpg" className="img_main_home" />
