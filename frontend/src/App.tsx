238339449a3b1f1b6f07520a3ddd65d9b61fb0ad
Hansel Rodriguez Vazquez <hansel.lunrak@gmail.com>
2025-08-03 08:03:52 +0200

    revision de la rama

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import PrivateRoute from "./components/PrivateRoute";
 import ManagePage from "./ManagePage";
 import Admin from "./Admin";
+import Login from "../pages/Login";
 
 function App() {
   return (
